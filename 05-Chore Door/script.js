810ca34880cf4c635586400f4962e5f98832bc4f
Steveen <steveen.echeverri@gmail.com>
2020-04-23 22:54:42 -0500

    Test function randon Chore Door Generator

--- 05-Chore Door/script.js
+++ 05-Chore Door/script.js
@@ -1,17 +1,38 @@
 let doorImage1 = document.getElementById('door1');
 let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
+let numCloseDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
 
 let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
 doorImage1.onclick = () => {
-  doorImage1.src= botDoorPath;
+  doorImage1.src= openDoor1;
 }
 doorImage2.onclick = () => {
-  doorImage2.src= beachDoorPath;
+  doorImage2.src= openDoor2;
 }
 doorImage3.onclick = () => {
-  doorImage3.src= spaceDoorPath;
+  doorImage3.src= openDoor3;
 }
+const randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random() * numCloseDoors);
+  if(choreDoor === 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else if (choreDoor === 1) { 
+    openDoor2 = botDoorPath;
+    openDoor1 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else {
+    openDoor3 = botDoorPath;
+    openDoor1 = beachDoorPath;
+    openDoor2 = spaceDoorPath;
+  }
+}
+randomChoreDoorGenerator();
