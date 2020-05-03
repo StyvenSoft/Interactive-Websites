0a0890038c1809957611516e96019df858547649
Steveen <steveen.echeverri@gmail.com>
2020-05-03 17:10:42 -0500

    Get data from Foursquare and weather URL

--- 11-Wanderlust/public/main.js
+++ 11-Wanderlust/public/main.js
@@ -1,11 +1,11 @@
 // Foursquare API Info
-const clientId = '';
-const clientSecret = '';
-const url = '';
+const clientId = 'L1AIHK3IXC341CPEDILYD2T522WWV5ZBJQWWAKWSAYXKE3ZW';
+const clientSecret = 'JBD23PM1JLENKJMRSWLKH1DRBOFEF0KOZER0UJTM44SO3M4Q';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
 // OpenWeather Info
-const openWeatherKey = '';
-const weatherUrl = '';
+const openWeatherKey = 'bd0344708607eb079f4546e6103f13ce';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Page Elements
 const $input = $('#city');
@@ -17,8 +17,22 @@ const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = () => {
-
+const getVenues = async () => {
+    const city = $input.val();
+    const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20180101`;
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            // console.log(response);
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+            console.log(venues);
+            return venues;
+          }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const getForecast = () => {
--- Readme.md
+++ Readme.md
@@ -7,3 +7,6 @@ Learn the Document Object Model, the interface between JavaScript and HTML eleme
 [Event reference](https://developer.mozilla.org/en-US/docs/Web/Events)
 
 [DOM Events with JavaScript](https://www.codecademy.com/learn/paths/web-development/tracks/build-interactive-websites/modules/dom-javascript-events/cheatsheet)
+
+[How To Use the JavaScript Developer Console](https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-developer-console)
+
